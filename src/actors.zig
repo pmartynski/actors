8927f83a79c8a7d1bc1f54a45033dbd4328a2f18
Piotr Martyński <p.martynski@gmail.com>
2024-08-06 00:32:30 +0200

    Actor docs

--- src/actors.zig
+++ src/actors.zig
@@ -4,22 +4,56 @@ const testing = std.testing;
 const c = @import("consumers.zig");
 const s = @import("channels.zig");
 
-// TODO: actor outbox
-// TODO: docs
+/// An actor consists of an inbox channel, a consumer. The inbox channel allows sending messages to the actor.
+/// The consumer processes the incoming messages using a handler function and updates the state accordingly.
+/// An Actor is guarantied to be single-threaded.
+///
+/// Example:
+/// ```
+///
+/// const MyState = struct { sum: u32 };
+///
+/// fn handle(msg: *const u32, state: *MyState) void {
+///     state.*.sum += msg.*;
+/// }
+///
+/// pub fn main() void {
+///   const initialState = &MyState { sum = 0 };
+///   var a = try Actor(MyState, u32).init(
+///       allocator,
+///       initialState,
+///       handle,
+///   );
+///   defer a.deinit();
+///   try a.run();
+///
+///   const inbox = a.inbox();
+///   _ = try inbox.send(1);
+/// }
+/// ```
 pub fn Actor(comptime S: type, comptime T: type) type {
     return struct {
-        name: []const u8,
         state: *S,
         inboxChannel: *s.SimpleChannel(T, 128),
         consumer: c.ChannelConsumer(T, S),
         thread: ?std.Thread,
         allocator: std.mem.Allocator,
 
+        /// Returns a channel writer for sending messages to the actor's inbox.
         pub fn inbox(self: *@This()) s.ChannelWriter(T) {
             return self.*.inboxChannel.channelWriter();
         }
 
-        pub fn init(allocator: std.mem.Allocator, name: []const u8, init_state: *S, handler: c.HandlerFn(T, S)) !@This() {
+        /// Initializes an actor with the specified state type `S`, message type `T`, and handler function.
+        ///
+        /// The `allocator` parameter is used to allocate memory for the actor's inbox channel and state.
+        /// The `init_state` parameter is a pointer to the initial state of the actor.
+        /// The `handler` parameter is a function that processes incoming messages and updates the state accordingly.
+        pub fn init(
+            allocator: std.mem.Allocator,
+            init_state: *S,
+            handler: c.HandlerFn(T, S),
+        ) !@This() {
             const channel = try allocator.create(s.SimpleChannel(T, 128));
             channel.* = try s.SimpleChannel(T, 128).init(allocator);
             const consumer = c.ChannelConsumer(T, S).create(
@@ -31,16 +65,17 @@ pub fn Actor(comptime S: type, comptime T: type) type {
                 .allocator = allocator,
                 .inboxChannel = channel,
                 .consumer = consumer,
-                .name = name,
                 .state = init_state,
                 .thread = null,
             };
         }
 
+        /// Starts the consumer thread
         pub fn run(self: *@This()) !void {
             self.*.thread = try self.*.consumer.run();
         }
 
+        /// Clears the allocated memory
         pub fn deinit(self: *@This()) void {
             self.inboxChannel.deinit();
             self.allocator.destroy(self.inboxChannel);
@@ -75,7 +110,6 @@ test "INT actor" {
     initialState.* = State.init(testing.allocator);
     var a = try Actor(State, Msg).init(
         testing.allocator,
-        "test_actor",
         initialState,
         H.handle,
     );
@@ -92,6 +126,4 @@ test "INT actor" {
 
     const actual = initialState.*.get(4);
     try testing.expectEqual(16, actual);
-
-    try testing.expectEqual("test_actor", a.name);
 }
